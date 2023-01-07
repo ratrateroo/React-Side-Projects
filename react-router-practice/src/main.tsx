e409fba9bbb8088b32c0c1f93753400ce5cd82bc
Mark_Leo <ratrateroo@gmail.com>
2023-01-07 23:54:52 +0800

    added error page

--- react-router-practice/src/main.tsx
+++ react-router-practice/src/main.tsx
@@ -3,11 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
+import ErrorPage from "./error-page";
 import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
