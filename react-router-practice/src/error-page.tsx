2a31b6c7444ecaa4d7e0646e2470fb34ecf6022d
Mark_Leo <ratrateroo@gmail.com>
2023-01-08 00:06:54 +0800

    fixed error page

--- react-router-practice/src/error-page.tsx
+++ react-router-practice/src/error-page.tsx
@@ -1,4 +1,4 @@
-import { useRouteError } from "react-router-dom";
+import { useRouteError, isRouteErrorResponse } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
@@ -8,9 +8,12 @@ export default function ErrorPage() {
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        {error instanceof Error && <i>{error.statusText || error.message}</i>}
-      </p>
+
+      {isRouteErrorResponse(error) ? (
+        <p>
+          <i>{error.statusText || error.data.message}</i>
+        </p>
+      ) : null}
     </div>
   );
 }
