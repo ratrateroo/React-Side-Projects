aa06d3bf41e979b7c99f3b5ccbbd187611a942a6
Mark_Leo <ratrateroo@gmail.com>
2023-01-09 23:49:08 +0800

    added route for contact

--- react-router-practice/src/main.tsx
+++ react-router-practice/src/main.tsx
@@ -4,12 +4,17 @@ import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from "./routes/root";
 import ErrorPage from "./error-page";
+import Contact from "./routes/contact";
 import "./index.css";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
     errorElement: <ErrorPage />,
+  },
+  {
+    path: "contacts/:contactId",
+    element: <Contact />,
   },
 ]);
 
