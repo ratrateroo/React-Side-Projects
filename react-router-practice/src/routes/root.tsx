095bb11a82b000afdf65160aed07ed8001db8bd8
Mark_Leo <ratrateroo@gmail.com>
2023-01-11 23:51:12 +0800

    added loader

--- react-router-practice/src/routes/root.tsx
+++ react-router-practice/src/routes/root.tsx
@@ -1,4 +1,13 @@
+import { Outlet, Link, useLoaderData } from "react-router-dom";
+
+import { getContacts } from "../contacts";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 export default function Root() {
+  const { contacts } = useLoaderData();
   return (
     <>
       <div>
@@ -21,15 +30,41 @@ export default function Root() {
         <nav>
           <ul>
             <li>
-              <a href={`contacts/1`}>Your Name</a>
+              <Link to={`contacts/1`}>Your Name</Link>
             </li>
             <li>
-              <a href={`contacts/2`}>Your Friend</a>
+              <Link to={`contacts/2`}>Your Friend</Link>
             </li>
           </ul>
         </nav>
+        <nav>
+          {contacts.length ? (
+            <ul>
+              {contacts.map((contact) => (
+                <li key={contact.id}>
+                  <Link to={`contacts/${contact.id}`}>
+                    {contact.first || contact.last ? (
+                      <>
+                        {contact.first} {contact.last}
+                      </>
+                    ) : (
+                      <i>No Name</i>
+                    )}{" "}
+                    {contact.favorite && <span>★</span>}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>
+              <i>No contacts</i>
+            </p>
+          )}
+        </nav>
       </div>
-      <div></div>
+      <div>
+        <Outlet />
+      </div>
     </>
   );
 }
