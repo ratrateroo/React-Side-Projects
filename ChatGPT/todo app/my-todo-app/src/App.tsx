011d43cc2c33d37c6a2036100083d4f9ad359a72
Mark_Leo <ratrateroo@gmail.com>
2023-02-01 02:35:53 +0800

    updated the todo app

--- ChatGPT/todo app/my-todo-app/src/App.tsx
+++ ChatGPT/todo app/my-todo-app/src/App.tsx
@@ -8,6 +8,7 @@ interface Todo {
 
 const App: React.FC = () => {
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [darkMode, setDarkMode] = useState(false);
 
   const handleTodoAdd = (text: string) => {
     setTodos([...todos, { id: Date.now(), text, complete: false }]);
@@ -23,19 +24,49 @@ const App: React.FC = () => {
     );
   };
 
+  const handleDarkModeToggle = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" placeholder="Add Todo" />
-        <button type="submit">Add</button>
-      </form>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} onClick={() => handleTodoToggle(todo)}>
-            {todo.text} {todo.complete ? "(completed)" : ""}
-          </li>
-        ))}
-      </ul>
+    <div
+      className={`bg-${darkMode ? "gray-800" : "gray-200"} text-${
+        darkMode ? "white" : "black"
+      }`}
+    >
+      <div className="container mx-auto py-10">
+        <button
+          className="bg-gray-400 px-4 py-2 mb-10"
+          onClick={handleDarkModeToggle}
+        >
+          {darkMode ? "Light Mode" : "Dark Mode"}
+        </button>
+        <form
+          className="bg-white p-6 rounded-lg shadow-md"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            className="w-full border border-gray-400 p-2 mb-4"
+            type="text"
+            placeholder="Add Todo"
+          />
+          <button className="bg-blue-500 text-white px-4 py-2">Add</button>
+        </form>
+        <ul className="mt-10">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="p-2 mb-2 border border-gray-400 rounded-lg"
+              onClick={() => handleTodoToggle(todo)}
+            >
+              {todo.text}
+              <span className="ml-2 text-sm text-gray-600">
+                {todo.complete ? "(completed)" : ""}
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
