5594ff8317e27de98575681c7b8d9b73757184ca
Mark_Leo <ratrateroo@gmail.com>
2023-02-11 23:56:48 +0800

    added loader function

--- react-router-practice-2/src/routes/root.jsx
+++ react-router-practice-2/src/routes/root.jsx
@@ -1,4 +1,11 @@
 import { Outlet, Link } from "react-router-dom";
+
+import { getContacts } from "../contacts";
+
+export async function loader() {
+  const contacts = await getContacts();
+  return { contacts };
+}
 export default function Root() {
   return (
     <>
