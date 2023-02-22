787f4d994914a1818a3422fca7bf917acfee4aea
Mark_Leo <ratrateroo@gmail.com>
2023-02-22 23:58:02 +0800

    added loader

--- react-router-practice-2/src/main.jsx
+++ react-router-practice-2/src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Contact from "./routes/contact";
-import Root from "./routes/root";
+import Root, { loader as rootLoader } from "./routes/root";
 import ErrorPage from "./error-page";
 import "./index.css";
 
@@ -10,6 +10,7 @@ const router = createBrowserRouter([
   {
     path: "/",
     errorElement: <ErrorPage />,
+    loader: rootLoader,
     children: [
       {
         path: "contacts/:contactId",
